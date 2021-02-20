1a331eae32864e171a9c8eb1f7c01de45d27724f
KostadinGorgive <Kostadin.Gorgive@outlook.com>
2021-02-20 14:00:00 +0100

    circleci

---
+++ cypress/tests/domDev.js
@@ -0,0 +1,19 @@
+import {
+  homepageTests,
+  navTests,
+  selectFixtureTests,
+  staticTests,
+} from '../support/testBlocks';
+
+describe('DOM dev', () => {
+  // WARNING: These tests are serial and share state
+  before(() => {
+    cy.clearStorage();
+    cy.visit('http://localhost:5000');
+  });
+
+  homepageTests();
+  navTests();
+  selectFixtureTests();
+  staticTests();
+});
